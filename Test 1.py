83b1ac2d37e1ae7f1a38ed9924a6ccde6a99275e
Sarah Stephenson <123026466+sstephenson01@users.noreply.github.com>
2023-02-22 16:03:55 -0500

    Add files via upload

---
+++ Test 1.py
@@ -0,0 +1,24 @@
+#Sarah Stephenson
+#Mid12
+def is_divisable_five(x):
+    if x % 5 ==0:
+        print("Yes,it is!")
+    else:
+        print("No it is not")
+is_divisable_five(5)
+
+#mid 14
+def next_stop(x):
+    print("The next stop is", x)
+    print("The next stop is", x)
+
+next_stop("Chicago")
+
+#Mid15
+for x in ["paper","rock","paper","scissor","rock","rock","scissor","paper"]:
+    if x == "rock":
+        print("rock hits scissors")
+    if x== "paper":
+        print("paper eats rock")
+    if x== "scissors":
+        print("scissors beats paper")
